52a4e69fde78ab80aeb50430c8dcd712c4216d5f
justmarsh <33488549+justmarsh@users.noreply.github.com>
2018-12-12 15:13:50 -0500

    Made some of the framework of the class

--- src/Test.java
+++ src/Test.java
@@ -5,7 +5,17 @@ public class Test
     public void newMain() {
         root.setData("Elephant");
 
+        System.out.println("Think of an animal.");
+    }
 
+    private void guess(LinkedBinaryTreeNode  node)
+    {
+
+    }
+
+    public void answerNo(LinkedBinaryTreeNode node)
+    {
+        System.out.print(" ");
     }
 
     public static void main(String[] args) {
