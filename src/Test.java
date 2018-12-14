d34f7f5002479a758ad98640619431c086af187d
justmarsh <33488549+justmarsh@users.noreply.github.com>
2018-12-14 11:26:41 -0500

    Made some of the framework of the class

--- src/Test.java
+++ src/Test.java
@@ -1,16 +1,36 @@
+import java.util.Scanner;
+
 public class Test
 {
     private LinkedBinaryTreeNode root;
 
     public void newMain() {
-        root.setData("Elephant");
-
+        Scanner input = new Scanner(System.in);
         System.out.println("Think of an animal.");
+        if(root.getData() == null)
+        {
+            setUp();
+            guess();
+        }
+
+
+
+
     }
 
-    private void guess(LinkedBinaryTreeNode  node)
+    private void setUp()
     {
+        root.setData("Elephant");
+    }
+    private void guess(char letter, LinkedBinaryTreeNode node)
+    {
+        if(letter == 'n')
+        {
+            if(node.getLeft() != null)
+            {
 
+            }
+        }
     }
 
     public void answerNo(LinkedBinaryTreeNode node)
@@ -20,7 +40,6 @@ public class Test
 
     public static void main(String[] args) {
         Test test = new Test();
-
         test.newMain();
     }
 }
